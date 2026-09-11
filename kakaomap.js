dcd51e005e0a67a3a5f46fa1ffeea4f2c3e3fa26
woojin <kevin2205@naver.com>
2026-09-11 20:49:34 +0900

    marker

--- kakaomap.js
+++ kakaomap.js
@@ -9,4 +9,14 @@ window.addEventListener("load", function () {
   };
 
   var map = new kakao.maps.Map(container, options);
+
+  var markerPosition = new kakao.maps.LatLng(35.196, 129.0794);
+
+  // 마커
+  var marker = new kakao.maps.Marker({
+    position: markerPosition,
+  });
+
+  // 마커가 지도 위에 표시
+  marker.setMap(map);
 });
